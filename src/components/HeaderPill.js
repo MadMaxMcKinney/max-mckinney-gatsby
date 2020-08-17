8a992ac8f5451f77babaaa2c264c1fa181d33d69
Max McKinney <maxgames101@gmail.com>
2020-08-16 21:40:52 -0500

    Update profile img

--- src/components/HeaderPill.js
+++ src/components/HeaderPill.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types';
 
-import MaxProfileImg from '../assets/img/maxmckinney-profile.png';
+import MaxProfileImg from '../assets/img/maxmckinney-profile-small.png';
 
 const HeaderPill = (props) => (
     <HeaderPillContainer>
