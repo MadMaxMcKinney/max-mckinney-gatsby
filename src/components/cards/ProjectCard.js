ff500fac28abbbbb66a0c740e78237c6117b5bc4
Max McKinney <maxgames101@gmail.com>
2020-10-14 22:59:03 -0500

    Update ProjectCard animation

--- src/components/cards/ProjectCard.js
+++ src/components/cards/ProjectCard.js
@@ -35,11 +35,9 @@ const ProjectCard = (props) => {
 const fadeIn = keyframes`
     from {
         opacity: 0;
-        transform: scale(0.9);
     }
     to {
         opacity: 1;
-        transform: scale(1);
     }
 `
 
