f7472b7506a9a1ea8ee7eb656c611ba61a511aa7
Max McKinney <maxgames101@gmail.com>
2021-04-18 14:16:11 -0500

    Change supportor row to two cols

--- src/components/page/SupporterMilestoneRow.js
+++ src/components/page/SupporterMilestoneRow.js
@@ -6,7 +6,7 @@ export const SupporterMilestoneRow = (props) => (
             <p className="text-gray-500"><i class="fad fa-calendar-alt mr-1"></i> {props.date}</p>
             <h1>{props.milestone}</h1>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-flow-col md:grid-cols-none gap-6">
+        <div className="grid grid-cols-2 md:grid-flow-col md:grid-cols-none gap-6">
             {props.children}  
         </div>
     </div>
