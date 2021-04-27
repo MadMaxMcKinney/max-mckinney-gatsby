910dc588f395310675bcd46185ef3faa37118072
Max McKinney <maxgames101@gmail.com>
2021-04-27 17:44:43 -0500

    Update email

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
 	<div className="animate-fade-in-slow px-6 mt-10 mb-10 mx-auto flex flex-col justify-center md:mx-auto md:flex-row md:justify-end md:mt-32 xl:px-28">
 
         {/* Email */}
-		<div className="text-xl text-center mb-10 font-bold md:text-2xl md:text-left md:mb-0" href="mailto:maxmckinney@hey.com">maxmckinney@hey.com</div>
+		<div className="text-xl text-center mb-10 font-bold md:text-2xl md:text-left md:mb-0" href="mailto:max@maxmckinney.com">max@maxmckinney.com</div>
 
         {/* Social Links */}
 		<div className="inline-grid grid-cols-3 gap-3 list-none justify-center md:hidden">
