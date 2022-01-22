7a4f0d6df7a2687f5057304fbada8d98b850752c
Max McKinney <maxgames101@gmail.com>
2022-01-21 21:24:48 -0600

    Remove outdated gatsby sources

--- src/pages/personal.js
+++ src/pages/personal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
 import { graphql, Link } from "gatsby"
+import DynamicLink from '../components/utils/DynamicLink'
 import Img from "gatsby-image"
 import {SIZE_MOBILE_LARGE} from '../components/CONSTANTS'
 
@@ -48,35 +49,6 @@ const SideProjectsPage = ({data}) => (
                 </SideProjectCard>
             ))}
         </SideProjectGrid>
-        
-        <MHeading03 className="text-white mt-28 mb-12 animate-fade-in-slow">design stuff from <PageLink className="text-max-pink-300" href="https://dribbble.com/MadMaxMcKinney">dribbble</PageLink></MHeading03>
-
-        <DribbbleGrid className="animate-fade-in-slow">
-            {data.allDribbbleShot.edges.map(({node}) => (
-                <a href={node.url}>
-                    <DribbblePost>
-                        <img src={node.localCover.publicURL}/>
-                    </DribbblePost>
-                </a>
-            ))}
-        </DribbbleGrid>
-
-        <MHeading03 className="text-white mt-28 mb-12 animate-fade-in-slow">writing stuff from <PageLink className="text-max-yellow-300" href="https://maxmckinney.medium.com/">medium</PageLink></MHeading03>
-
-        <MediumPostGrid className="animate-fade-in-slow">
-            {data.allMediumPost.edges.map(({node}) => (
-                <MediumPost href={"https://maxmckinney.medium.com/" + node.slug + "-" + node.medium_id}>
-                    <div>
-                        <img src={`https://miro.medium.com/max/1000/${node.virtuals.previewImage.imageId}`} alt="Article preview image"/>
-                    </div>
-                    <div className="grid">
-                        <MBody><strong>{node.title}</strong></MBody>
-                        <MBodyLight>{node.virtuals.subtitle}</MBodyLight>
-                    </div>
-                </MediumPost>
-            ))}
-        </MediumPostGrid>
-        
 
     </PageGrid>
 )
@@ -116,7 +88,7 @@ const SideProjectGrid = styled.div`
     }
 `
 
-const SideProjectCard = styled(Link)`
+const SideProjectCard = styled(DynamicLink)`
     --accent-color: ${props => props.accent};
     
     display: flex;
@@ -185,139 +157,6 @@ const SideProjectLink = styled(MBodyLight)`
     color: var(--accent-color);
 `
 
-const DribbbleGrid = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(290px, 1fr));
-    grid-gap: 24px;
-
-    a {
-        line-height: 0;
-    }
-
-    @media(max-width: ${SIZE_MOBILE_LARGE}) {
-        grid-template-columns: 10px; /* Explicit grid, defining the first item */
-        grid-auto-flow: column;
-        grid-auto-columns: 75%; /* Implicit grid, defining the remaining items */
-        overflow-x: scroll;
-        grid-column: 1/4;
-        grid-gap: 16px;
-        padding-bottom: 16px;
-        
-        /* This is needed to provide an air gap before and after the items in the horizontally scrolling list */
-        &::after, &::before{
-            content: '';
-            width: 10px;
-        }
-    }
-`
-
-const DribbblePost = styled.div`
-    --border-radius: 4px;
-
-    border-radius: var(--border-radius);
-    background: var(--blueblack-500);
-    padding: 4px;
-
-    position: relative;
-
-    transition-property: all;
-    transition-duration: 0.15s;
-
-    img {
-        width: 100%;
-    }
-
-    // Hover
-    &:hover {
-        transform: scale(1.03);
-        background: white;
-        color: black;
-    }
-
-    // Active (Pressed)
-    &:active {
-        transform: scale(1);
-    }
-
-    @media(max-width: ${SIZE_MOBILE_LARGE}) {
-        &:hover {
-            transform: scale(1);
-        }
-        &:active {
-            transform: scale(0.98);
-        }
-    }
-`
-
-const MediumPostGrid = styled.div`
-    display: grid;
-    grid-auto-flow: row;
-    grid-gap: 32px;
-    align-items: flex-start;
-
-    @media(max-width: ${SIZE_MOBILE_LARGE}) {
-        margin-bottom: 32px;
-    }
-`
-
-const MediumPost = styled.a`
-    display: grid;
-    grid-template-columns: 175px 1fr;
-    grid-gap: 24px;
-    font-size: 1rem;
-    border-radius: 4px;
-    height: 100%;
-
-    position: relative;
-    border: 1px solid transparent;
-
-    transition: all 0.1s;
-
-    p {
-        margin: 0;
-        max-width: 650px;
-        font-weight: 400;
-    }
-    
-    img {
-        width: 100%;
-        height: 100px;
-        border-radius: 4px;
-    }
-
-    &::after {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        margin: -8px;
-        border-radius: 4px;
-        border: 1px solid transparent;
-
-        transition: border 0.1s;
-    }
-
-    &:hover&::after {
-        border: 1px solid white;
-    }
-
-    &:active {
-        transform: scale(0.99);
-    }
-
-    @media(max-width: 710px) {
-        grid-template-columns: 1fr;
-        grid-gap: 16px;
-
-        img {
-            object-fit: cover;
-            height: 200px;
-        }
-    }
-`
-
 export default SideProjectsPage
 
 export const query = graphql`
@@ -355,33 +194,6 @@ query personalProjectQuery {
                 }
               }
             }
-          }
-        }
-    }
-    allDribbbleShot(limit: 9) {
-        edges {
-            node {
-                url
-                localCover {
-                  publicURL
-                }
-                title
-            }
-        }
-    }
-    allMediumPost(sort: { fields: [createdAt], order: DESC }, limit: 4) {
-        edges {
-          node {
-            id
-            title
-            virtuals {
-              subtitle
-              previewImage {
-                imageId
-              }
-            }
-            medium_id
-            slug
           }
         }
     }
