d79c9692023a8ff7aefbc3ddc1815749b4298a8a
Max McKinney <maxgames101@gmail.com>
2022-12-22 16:08:38 -0600

    Margin to Flex Gap

--- src/components/buttons/ReadMoreButton.js
+++ src/components/buttons/ReadMoreButton.js
@@ -5,8 +5,8 @@ import { MHeading05 } from '../typography'
 
 const ReadMoreButton = (props) => {
   return (
-	<ButtonContainer to={props.link} accent={props.accent} className="flex w-fit group bg-gray-600 items-center justify-center rounded px-4 py-2 border border-solid border-transparent filter transition transform hover:brightness-110 hover:border-white active:scale-95">
-        <MHeading05 className="mr-3">{props.children}</MHeading05><i className="far fa-arrow-right group-hover:animate-pulse-right"></i>
+	<ButtonContainer to={props.link} accent={props.accent} className="flex w-fit gap-4 group bg-gray-600 items-center justify-center rounded px-4 py-2 border border-solid border-transparent filter transition transform hover:brightness-110 hover:border-white active:scale-95">
+        <MHeading05>{props.children}</MHeading05><i className="far fa-arrow-right group-hover:animate-pulse-right"></i>
 	</ButtonContainer>
   )
 }
