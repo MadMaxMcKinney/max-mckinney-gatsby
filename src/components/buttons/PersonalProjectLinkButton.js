f6e2802dbd9b3206fe2ea0b25e48461abeaf2e0f
Max McKinney <maxgames101@gmail.com>
2022-12-24 10:48:41 -0600

    Remove StyledComps for PersonalButtons

--- src/components/buttons/PersonalProjectLinkButton.js
+++ src/components/buttons/PersonalProjectLinkButton.js
@@ -1,23 +1,19 @@
 import React from "react";
-import styled from "styled-components";
 import { MBody } from "../typography";
 import { RocketLaunchIcon } from "@heroicons/react/20/solid";
 
 const PersonalProjectLinkButton = (props) => {
     return (
-        <ButtonContainer
+        <a
             href={props.href}
             accent={props.accent}
-            className={`flex w-fit gap-2 items-center justify-center rounded-full px-4 py-2 border border-solid border-transparent transition transform hover:border-white active:scale-95`}
+            style={{'--accentColor': props.accent}}
+            className={`flex w-fit gap-2 items-center justify-center rounded-full px-4 py-2 bg-[var(--accentColor)] border border-solid border-transparent transition transform hover:border-white active:scale-95`}
         >
             <RocketLaunchIcon className="w-5 h-5"></RocketLaunchIcon>
             <MBody>View project</MBody>
-        </ButtonContainer>
+        </a>
     );
 };
-
-const ButtonContainer = styled("a")`
-    background: ${(props) => props.accent};
-`;
 
 export default PersonalProjectLinkButton;
--- src/components/buttons/PersonalProjectLinkSourceButton.js
+++ src/components/buttons/PersonalProjectLinkSourceButton.js
@@ -1,25 +1,19 @@
 import React from "react";
-import styled from "styled-components";
 import { MBody } from "../typography";
 import { CodeBracketIcon } from "@heroicons/react/20/solid";
 
 const PersonalProjectLinkSourceButton = (props) => {
     return (
-        <ButtonContainer
+        <a
             href={props.href}
             accent={props.accent}
-            className="flex w-fit gap-2 bg-white text-black items-center justify-center rounded-full px-4 py-2 border border-solid border-transparent transition transform hover:border-white hover:text-white active:scale-95"
+            style={{'--accentColor': props.accent}}
+            className="flex w-fit gap-2 bg-white text-black items-center justify-center rounded-full px-4 py-2 border border-solid border-transparent transition transform hover:border-white hover:text-white hover:bg-[var(--accentColor)] active:scale-95"
         >
             <CodeBracketIcon className="w-5 h-5"></CodeBracketIcon>
             <MBody>View source</MBody>
-        </ButtonContainer>
+        </a>
     );
 };
-
-const ButtonContainer = styled("a")`
-    &:hover {
-        background: ${(props) => props.accent};
-    }
-`;
 
 export default PersonalProjectLinkSourceButton;
