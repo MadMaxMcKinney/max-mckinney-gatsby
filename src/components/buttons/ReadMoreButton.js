60ac372fa8293b60e4fc8a39e3c4fc54d26302f4
Max McKinney <maxgames101@gmail.com>
2022-12-24 11:13:33 -0600

    Simplified some StyledComponents into Tailwind classes

--- src/components/buttons/ReadMoreButton.js
+++ src/components/buttons/ReadMoreButton.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import {Link} from 'gatsby'
-import styled from 'styled-components'
-import { MHeading05 } from '../typography'
-import {ArrowRightIcon} from '@heroicons/react/20/solid'
+import React from "react";
+import { Link } from "gatsby";
+import { MHeading05 } from "../typography";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
 
 const ReadMoreButton = (props) => {
-  return (
-	<ButtonContainer to={props.link} accent={props.accent} className={`flex w-fit gap-2 group bg-gray-600 items-center justify-center rounded px-4 py-2 border border-solid border-transparent transition transform hover:bg-${props.accent} hover:border-white active:scale-95`}>
-        <MHeading05>{props.children}</MHeading05><ArrowRightIcon className="w-4 h-4 group-hover:animate-pulse-right"></ArrowRightIcon>
-	</ButtonContainer>
-  )
-}
-
-const ButtonContainer = styled(Link)`
-    &:hover{
-        background: ${props => props.accent};
-    }
-`;
+    return (
+        <Link
+            to={props.link}
+            accent={props.accent}
+            style={{'--accentColor': props.accent}}
+            className={`flex w-fit gap-2 group bg-gray-600 items-center justify-center rounded px-4 py-2 border border-solid border-transparent transition transform hover:bg-[var(--accentColor)] hover:border-white active:scale-95`}
+        >
+            <MHeading05>{props.children}</MHeading05>
+            <ArrowRightIcon className="w-4 h-4 group-hover:animate-pulse-right"></ArrowRightIcon>
+        </Link>
+    );
+};
 
 export default ReadMoreButton;
